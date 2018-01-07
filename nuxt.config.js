debf246dd0f22c3915cf424dbd7385900ec78f6f
duqi <916475145@qq.com>
2018-01-07 22:26:01 +0800

    .gitignore

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const vuxLoader = require('vux-loader')
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    vendor: ['axios'],
+    babel:{ 
+        "presets": ["es2015", "stage-0"]
+    },
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          // loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+      vuxLoader.merge(config, {
+        plugins: ['vux-ui', 'progress-bar', 'duplicate-style']
+      })      
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+
+   <box gap="10px 10px">
+      <icon type="success"></icon>
+      <icon type="info"></icon>
+      <icon type="info-circle"></icon>
+      <icon type="warn"></icon>
+      <icon type="waiting"></icon>
+      <icon type="waiting-circle"></icon>
+      <icon type="safe-success"></icon>
+      <icon type="safe_warn"></icon>
+      <icon type="success-circle"></icon>
+      <icon type="success-no-circle"></icon>
+      <icon type="circle"></icon>
+      <icon type="download"></icon>
+      <icon type="cancel"></icon>
+      <icon type="search"></icon>
+      <icon type="clear"></icon>
+      <br/>
+      <icon type="success" is-msg></icon>
+      <icon type="info" is-msg></icon>
+      <icon type="warn" is-msg></icon>
+      <icon type="waiting" is-msg></icon>
+      <icon type="safe_success" is-msg></icon>
+      <icon type="safe_warn" is-msg></icon>
+    </box>
+
+  </div>
+</template>
+<script>
+  import { Box, Icon } from 'vux'
+  export default {
+    components: {
+    Box,
+    Icon
+  },
+    data() {
+      return {
+        value1: null,
+        value2: null,
+        color3: 'rgba(19, 206, 102, 0.8)',
+         formInline: {
+          user: '',
+          region: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    },
+    mounted:function(){
+        // console.log(vux+"-----");
+    }
+  }
+
+</script>
+<style>
+.color{
+  /*color: red;*/
+}
+</style>
